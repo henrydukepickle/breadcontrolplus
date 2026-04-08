90c7f7860d10b4e8c314f49b7c741c3dd2f59a3d
kwaliuh <147679331+kwaliuh@users.noreply.github.com>
2026-04-08 02:15:56 -0400

    TIMER

--- src/app.rs
+++ src/app.rs
@@ -1,14 +1,15 @@
 use eframe::egui::CentralPanel;
 
-use crate::game::Game;
+use crate::{game::Game, timed_button::Timer};
 
 pub struct App {
     gs: Game,
+    gain_timer: Timer
 }
 
 impl App {
     pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
-        Self { gs: Game::new() }
+        Self { gs: Game::new(), gain_timer: Timer::new(2.0) }
     }
 }
 
@@ -18,8 +19,9 @@ impl eframe::App for App {
             ui.label(format!("crobits: {}", self.gs.crobits));
             ui.label(format!("Crobbits: {}", self.gs.crobbits_big));
             ui.label(format!("Crobbit size: {}", self.gs.crobbit_size));
-            if ui.button("gain").clicked() {
+            if ui.button("gain").clicked() && self.gain_timer.done() {
                 self.gs.gain();
+                self.gain_timer.start();
             }
             
         });
